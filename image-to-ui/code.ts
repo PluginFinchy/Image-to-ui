949e42d1c488ae1e42ce9fa6f49d2c1f902b6d37
Luke Finch <lukechristianfinch@gmail.com>
2021-01-18 18:30:46 +0000

    Upload tutorial

---
+++ image-to-ui/code.ts
@@ -0,0 +1,25 @@
+
+figma.showUI(__html__);
+
+let exportNode = figma.currentPage.selection[0]
+
+exportNode.exportAsync({
+  format: "PNG",
+  constraint: {
+    type: "SCALE",
+    value: 2,
+}}
+).then(
+  resolved => {
+    sendToUi(resolved)
+  },
+  rejected => {
+    console.error(rejected)
+    figma.notify('Promise was rejected')
+  }
+)
+
+
+function sendToUi(imagedata){
+  figma.ui.postMessage({type: 'exportImage', data: imagedata})
+}
